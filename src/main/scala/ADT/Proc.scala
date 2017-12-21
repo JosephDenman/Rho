6300e7ca121bb33996c9e9b1d2e52b32506815b9
JosDenmark <josephrileydenman@gmail.com>
2017-12-21 18:54:56 -0500

    refactored store for monadic IO

--- src/main/scala/ADT/Proc.scala
+++ src/main/scala/ADT/Proc.scala
@@ -28,7 +28,7 @@ sealed trait Proc[Chan]
   // *X
   case class Drop[Chan](x: Chan) extends Proc[Chan]
 
-/**
+/*
  * The uninhabited type.
  */
 case class Void(z: Void)
@@ -38,6 +38,7 @@ object Void {
   /**
    * Logical reasoning of type 'ex contradictione sequitur quodlibet'
    */
+
   def absurd[A](z: Void): A = absurd(z)
 
   def vacuous[F[_], A](fa: F[Void], z: Void)(implicit F: Functor[F]): F[A] = F.map(fa)(absurd(z))
--- src/main/scala/AbstractInterpreter/Comm.scala
+++ src/main/scala/AbstractInterpreter/Comm.scala
@@ -1,27 +1,6 @@
 package ADT
 
-import AbstractInterpreter.Aliases.{Channel, Env}
-import AbstractInterpreter.PureStoreLike
 import cats.data.ReaderT
 import monix.eval.{MVar, Task}
 
 case class Comm[A](run: ReaderT[Task,Map[String,MVar[Rho]],A])
-
-case class _Input[Rho](x: Channel[Rho], z: Rho, k: Proc[Rho], env: Env[Channel[Rho]])
-
-object Evaluator {
-
-  val consumer: _Input[Rho] => Task[Proc[String]] = {
-    proc =>
-      // alloc for z
-      // read from x
-      // bind that to z
-      // update environment for k
-
-
-      // need to implement mutable references???
-      for { addr <- PureStoreLike.mvarStore.alloc(proc.z)
-            actual <- PureStoreLike.mvarStore.read(proc.x)
-            rho <- PureStoreLike.mvarStore.bind(addr)(actual)} yield {}
-  }
-}
